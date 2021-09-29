a34e8fae83da43644f8adfdae23e9790007656b5
Denis Ivanov <90346222+theredcapes@users.noreply.github.com>
2021-09-29 22:26:22 +0300

    Add files via upload

---
+++ les3.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class les3 {
+    private static Scanner sc;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s1, s2;
+        s1 = sc.nextLine();
+        s2 = sc.nextLine();
+        char[] chArr1 = s1.toUpperCase().toCharArray();
+        char[] chArr2 = s2.toUpperCase().toCharArray();
+        for (int i = chArr1.length - 1; i > 0; i--)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                if (chArr1[j] > chArr1[j + 1])
+                {
+                    char tmp = chArr1[j];
+                    chArr1[j] = chArr1[j + 1];
+                    chArr1[j + 1] = tmp;
+                }
+            }
+        }
+        for (int i = chArr2.length - 1; i > 0; i--)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                if (chArr2[j] > chArr2[j + 1])
+                {
+                    char tmp = chArr2[j];
+                    chArr2[j] = chArr2[j + 1];
+                    chArr2[j + 1] = tmp;
+                }
+            }
+        }
+        String valueOfchar1 = String.valueOf(chArr1);
+        String valueOfchar2 = String.valueOf(chArr2);
+        valueOfchar1 = valueOfchar1.replaceAll(" ", "");
+        valueOfchar2 = valueOfchar2.replaceAll(" ", "");
+        System.out.println(valueOfchar1);
+        System.out.println(valueOfchar2);
+        System.out.println(valueOfchar1.equalsIgnoreCase(valueOfchar2));
+
+    }
+}
